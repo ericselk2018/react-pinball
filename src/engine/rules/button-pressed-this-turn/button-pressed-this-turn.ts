dd3fa54974b368e912903f4d5dd32e24084174af
ericselkpc <eric.selk@paulaschoice.com>
2023-03-28 16:55:27 -0700

    syntax change only

--- src/engine/rules/button-pressed-this-turn/button-pressed-this-turn.ts
+++ src/engine/rules/button-pressed-this-turn/button-pressed-this-turn.ts
@@ -1,9 +1,7 @@
-import Game from '@/engine/entities/Game';
 import Rule from '@/engine/entities/Rule';
 
 // Mark button as pressed in the current turn.
-const buttonPressed: Rule = (args: { game: Game }) => {
-	const { game } = args;
+const buttonPressed: Rule = ({ game }) => {
 	const { status, pressedButton, buttonsPressedThisTurn, log } = game;
 
 	if (status !== 'playing' || !pressedButton) {
