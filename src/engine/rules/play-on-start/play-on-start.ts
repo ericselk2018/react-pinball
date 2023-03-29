b82102370aad5efa3f5a884e99fea42a8de17c6a
ericselkpc <eric.selk@paulaschoice.com>
2023-03-29 08:23:20 -0700

    fix bug causing credits to consume on each turn instead of only each game

--- src/engine/rules/play-on-start/play-on-start.ts
+++ src/engine/rules/play-on-start/play-on-start.ts
@@ -6,28 +6,33 @@ import Rule from '@/engine/entities/Rule';
 const playOnStart: Rule = ({ game }) => {
 	const { pressedButtons, status, pressedButton, tapCoil, log, showingMenu, creditsNeeded } = game;
 
-	if (status !== 'readyToPlay' && status !== 'waitingForNextPlayer') {
-		return;
-	}
-
-	if (showingMenu !== 'game-setup' && status !== 'waitingForNextPlayer') {
-		return;
-	}
-
-	if (
+	const pressedStartWithBallReadyToEject =
 		pressedButton?.id === startButtonButton.id &&
-		pressedButtons.some((pressedButton) => pressedButton.id === troughBallOneButton.id) &&
-		creditsNeeded <= 0
-	) {
-		game.status = 'playing';
-		game.showingMenu = undefined;
-		tapCoil({ coil: troughBallEjectCoil });
-		game.ballsInPlay++;
-		game.currentPlayer.ballsUsed++;
-		game.credits -= game.creditsRequired;
-		game.startTurn();
-		log('started play');
+		pressedButtons.some((pressedButton) => pressedButton.id === troughBallOneButton.id);
+	if (!pressedStartWithBallReadyToEject) {
+		return;
 	}
+
+	const newGame = status === 'readyToPlay' && showingMenu === 'game-setup';
+	const nextPlayer = status === 'waitingForNextPlayer' && !showingMenu;
+	if (!newGame && !nextPlayer) {
+		return;
+	}
+
+	if (newGame) {
+		if (creditsNeeded > 0) {
+			return;
+		}
+		game.credits -= game.creditsRequired;
+	}
+
+	game.status = 'playing';
+	game.showingMenu = undefined;
+	tapCoil({ coil: troughBallEjectCoil });
+	game.ballsInPlay++;
+	game.currentPlayer.ballsUsed++;
+	game.startTurn();
+	log('started play');
 };
 
 export default playOnStart;
