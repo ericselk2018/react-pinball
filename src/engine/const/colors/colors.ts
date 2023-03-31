d1d4860b4d8fdedc232ebab6114396cb438698fe
ericselkpc <eric.selk@paulaschoice.com>
2023-03-30 17:27:35 -0700

    updating debonaire light show

--- src/engine/const/colors/colors.ts
+++ src/engine/const/colors/colors.ts
@@ -1,7 +1,8 @@
 import Color from '@/engine/entities/Color';
 
-export const black: Color = { bluePercent: 0, greenPercent: 0, redPercent: 0 };
+export const black: Color = { redPercent: 0, greenPercent: 0, bluePercent: 0 };
 export const off = black;
 
-export const red: Color = { bluePercent: 0, greenPercent: 0, redPercent: 1 };
-export const blue: Color = { bluePercent: 1, greenPercent: 0, redPercent: 0 };
+export const red: Color = { redPercent: 0.8, greenPercent: 0.2, bluePercent: 0 };
+export const green: Color = { redPercent: 0.2, greenPercent: 0.8, bluePercent: 0.2 };
+export const blue: Color = { redPercent: 0, greenPercent: 0.2, bluePercent: 0.8 };
--- src/engine/const/lights/lights.ts
+++ src/engine/const/lights/lights.ts
@@ -98,4 +98,7 @@ const lights: Light[] = [
 	leftKickerLight,
 ];
 
+// TODO: remove slice once all LEDs are wired -- waiting for out-of-stock .100 crimp connectors
+export const lightsInCounterClockwiseOrder = lights.slice(0, 10);
+
 export default lights;
